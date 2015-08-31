2b41e2823d51cd5a588469287d1b79ba1ecaf0fa
coq219 <coq219@lehigh.edu>
2015-08-31 23:08:54 +0000

    yes

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+//Conor Quinlan
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+
+//  add main method
+  public static void main(String[] args) {
+      
+//  print the statement with box around it
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("^  ^  ^  ^  ^  ^");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-C--O--Q--2--1--9->");
+    System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("v  v  v  v  v  v");
+    System.out.println("I'm from Columbus, Ohio and I have a large family.");
+    System.out.println("This Summer I worked at a software company and that's what got me interested in Computer Science.");
+
+  }
+}
